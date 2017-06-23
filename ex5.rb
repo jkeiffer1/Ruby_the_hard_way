e54a0f57f1fdd6bc578c5e13eacf0edbf80c5550
jkeiffer1 <saros82@hotmail.com>
2017-06-23 13:45:17 -0400

    Variables and printing, fixed 2 spelling errors.

--- ex5.rb
+++ ex5.rb
@@ -8,10 +8,10 @@ my_hair = 'Brown'
 
 puts "Let's talk about #{my_name}."
 puts "He's #{my_height} inches tall."
-puts "He's #{my_weight} punds heavy."
+puts "He's #{my_weight} pounds heavy."
 puts "Actually that's not too heavy."
 puts "He's got #{my_eyes} eyes and #{my_hair} hair."
 puts "His teeth are usualy #{my_teeth} depending on the coffee."
 
 # this line is tricky, try to get it exactly right
-puts "If i add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
